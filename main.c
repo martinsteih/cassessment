5ac887ca2c2a61638ffa4ddb80262979412b699d
Martin Steih <martinsteih@gmx.de>
2022-10-14 05:59:01 -0600

    State machine state created

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 
 #include "object.h"
 
+typedef enum { IDLE, MOVING, ERROR, MAX_STATES } EState;
+
+typedef struct {
+  EState _current;
+  EState _new;
+  uint8_t _internal_event_generated;
+} sm_motor_t;
+
 int main(int argc, char **argv) {
   OBJECT(obj);
   printable_Print((printable_t *const)(&obj));
